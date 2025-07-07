8835b58ec7dbe49d82f1307b93241e590b6e3d84
FrancisMunezero <190288011+FrancisMunezero@users.noreply.github.com>
2025-07-07 03:05:10 -0400

    Initial QA Portfolio Website

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleProjects(){
+    var projectSection = document.getElementById("projectList");
+    if (projectSection.style.display === "none") {
+        projectSection.style.display = "block";
+    } else {
+        projectSection.style.display = "none";
+    }
+    }
